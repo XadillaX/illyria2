e1699c492bb8b75fbe18915525687d3470f0bd32
libin <bin.li@upai.com>
2014-07-06 21:15:44 +0800

    update socket to hacked nssocket to support reconnect

--- example/client.js
+++
@@ -1,8 +0,0 @@
-var connection = require('../lib').createConnection();
-
-connection.connect(8888, 'localhost', function(client) {
-    client.call('Number.add', 1, 2, function(err, result) {
-        console.log(err, result);
-        process.exit(0);
-    });
-});
