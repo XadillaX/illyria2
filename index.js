76d8ddd374898c18b0e11d94bed19d0b69f21478
XadillaX <admin@xcoder.in>
2015-03-04 17:22:00 +0800

    fix some bugs and add tests

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * XadillaX created at 2015-03-04 16:07:24
+ *
+ * Copyright (c) 2015 Huaban.com, all rights
+ * reserved
+ */
+require("sugar");
+exports.helper = require("./lib/helper");
+exports.ISocket = require("./lib/isocket");
+exports.Zookeeper = require("./lib/zookeeper");
+exports.Server = require("./lib/server");
+
+exports.createServer = exports.Server.createServer;
+
